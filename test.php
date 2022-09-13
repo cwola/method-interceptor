cc7cecab8e62fbde53d41ce7ba0b1b28ec29116c
cwola <buraito@outlook.com>
2022-09-13 20:28:54 +0900

    v0.1.0

--- test.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-require __DIR__ . DIRECTORY_SEPARATOR . 'vendor' . DIRECTORY_SEPARATOR . 'autoload.php';
-
-require __DIR__ . DIRECTORY_SEPARATOR . 'test.class.php';
-
-$test = new TEST;
-$test->test3();
-
